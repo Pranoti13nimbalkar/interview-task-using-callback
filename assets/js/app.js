476a173314a62dc2852e0489966faf67485d2d3f
pranoti <you@example.comghotekarpranoti@gmail.com>
2025-09-07 18:36:58 +0530

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,62 +1,88 @@
   let cl= console.log;
 
-const snackbar=(masg , icon)=>{
-   Swal.fire({
-  title: masg,
-  icon:icon,
-  timer:2500
-  
-});
+ const snackbar =(masg, icon)=>{
+  swal.fire({
+    title:masg,
+    icon:icon,
+    timer:1500
+  })
+ }
+
+
+const hrCall = ()=>{
+  return new Promise((resolve, reject)=>{
+     setTimeout(()=>{
+    let err = Math.random() >= .3 ? false : true;
+    if(!err){
+     resolve('Candidate is shortlisted for 1st techRound !!!')
+    }else{
+    reject('Profile does not match', 'error')
+    }
+  }, 1500)
+  })
+ 
 }
-  const hrCall=()=>{
-    setTimeout(()=>{
-        let err=Math.random() >= .3 ? false : true;
-        if(!err){
-            cl('candidate shortlisted for 1st round')
-             firstTechRound();
-        }else{
-           snackbar('Profile does not match', 'error')
-        }
-    },1500)
-  }
 
-  
-   
-   const firstTechRound=()=>{
-     setTimeout(()=>{
-        let err=Math.random() >= .3 ? false : true;
-        if(!err){
-            cl('candidate shortlisted for 2st round')
-            secondTechRound()
-        }else{
-            snackbar('candidate is not good in basics', 'error')
-        }
-    },1200)
-   }
-  
-   
-    
-   const secondTechRound=()=>{
-     setTimeout(()=>{
-        let err=Math.random() >= .3 ? false : true;
-        if(!err){
-            cl('candidate shortlisted for manager level round')
-            mlRound()
-        }else{
-            snackbar('candidate is struglling to write a code', 'error')
-        }
-    },1000)
-   }
-    const mlRound=()=>{
-     setTimeout(()=>{
-        let err=Math.random() >= .3 ? false : true;
-        if(!err){
-          snackbar('candidate is selected for given profile', 'success')
-            
-        }else{
-            snackbar('candidate is fail to explain previous project', 'error')
-        }
-    },700)
-   }
+const firstTechInter =()=>{
+  return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+    let err = Math.random() >= .3 ? false : true;
+    if(!err){
+     resolve('Candidate is shortlisted for 2nd techRound !!!')
+      
+    }else{
+      reject('candidate is not good in basics', 'error')
+    }
+  }, 1200)
+  })
 
-  hrCall();
+}
+
+const secondTechInter =()=>{
+  return new Promise((resolve, reject)=>{
+     setTimeout(()=>{
+    let err = Math.random() >= .3 ? false : true;
+    if(!err){
+    resolve('Candidate is shortlisted for mlRound !!!')
+     
+    }else{
+      reject('candidate is struglling to write code', 'error')
+    }
+  }, 1000)
+  })
+  
+}
+
+const mlRound =()=>{
+  return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+    let err = Math.random() >= .3 ? false : true;
+    if(!err){
+   resolve('Candidate is selected for given profile !!!', 'success')
+    }else{
+      reject('candidate is not able to explain privious project', 'error')
+    }
+  }, 800)
+  })
+  
+}
+
+hrCall()
+ .then(res=>{
+   cl(res);
+   return  firstTechInter()
+ })
+   .then(res=>{
+    cl(res)
+    return secondTechInter()
+   })
+   .then(res=>{
+    cl(res)
+    return mlRound()
+   })
+   .then(res=>{
+    snackbar(res , 'success')
+   })
+   .catch(err=>{
+    snackbar(err, 'error')
+   })
